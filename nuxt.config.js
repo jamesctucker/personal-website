a50361202c8f86aca6f92af32e1ea8140541f89f
James C. Tucker <hellojamestucker@gmail.com>
2022-08-28 13:49:19 -0500

    install content and fonts modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+  ],
 });
